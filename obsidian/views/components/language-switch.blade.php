8bcb037994f0d6e31a611625748a65372748a1c6
Eiro.tf <freakgaming2009@gmail.com>
2026-02-16 19:41:06 +0530

    Delete obsidian/views directory

--- obsidian/views/components/language-switch.blade.php
+++
@@ -1,8 +0,0 @@
-<x-select
-    wire:model.live="currentLocale"
-    :options="collect($locales)->map(fn($locale, $code) => [
-        'value' => $code,
-        'label' => $locale
-    ])->values()->toArray()"
-    placeholder="Select language"
-/>
